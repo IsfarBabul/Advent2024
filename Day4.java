8a836874320fcea9e6c25caada2a48c9e130d4da
IsfarBabul <145706698+IsfarBabul@users.noreply.github.com>
2024-12-16 00:17:21 -0500

    Add files via upload

--- Day4.java
+++ Day4.java
@@ -1,17 +1,17 @@
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Day4 {
     public static void main(String[] args) {
 
-        //ArrayList<String> fileData = getFileData("src/Day4Input.txt");
-        ArrayList<String> fileData = getFileData("src/exampletext");
-        System.out.println(fileData);
+        ArrayList<String> fileData = getFileData("src/Day4Input.txt");
+        //ArrayList<String> fileData = getFileData("src/exampletext");
+        //ArrayList<String> fileData = getFileData("src/dummyText");
+        //System.out.println(fileData);
 
         ArrayList<ArrayList<String>> fileData2D = new ArrayList<ArrayList<String>>();
 
@@ -19,10 +19,9 @@ public class Day4 {
             fileData2D.add(convertWordToStrings(fileData.get(i)));
         }
 
-        System.out.println(fileData2D);
+        //System.out.println(fileData2D);
 
         System.out.println(convertWordToStrings("XMAS"));
-        System.out.println(checkDiagonallyLeft(convertWordToStrings("XMAS"), fileData2D));
 
         int[] unsimplifiedAnswer = {checkHorizontally(convertWordToStrings("XMAS"), fileData2D), checkVertically(convertWordToStrings("XMAS"), fileData2D), checkDiagonallyRight(convertWordToStrings("XMAS"), fileData2D), checkDiagonallyLeft(convertWordToStrings("XMAS"), fileData2D), checkHorizontally(convertWordToStrings("SAMX"), fileData2D), checkVertically(convertWordToStrings("SAMX"), fileData2D), checkDiagonallyRight(convertWordToStrings("SAMX"), fileData2D), checkDiagonallyLeft(convertWordToStrings("SAMX"), fileData2D)};
         System.out.println(Arrays.toString(unsimplifiedAnswer));
@@ -30,10 +29,59 @@ public class Day4 {
         for (int i = 0; i < unsimplifiedAnswer.length; i++) {
             simplifiedAnswer += unsimplifiedAnswer[i];
         }
-        System.out.println(simplifiedAnswer);
+        System.out.println("Your answer to Advent 2024 Day 4 Part 1 is: " + simplifiedAnswer);
+        System.out.println("Your answer to Advent 2024 Day 4 Part 2 is: " + checkXMas(fileData2D));
 
         // you now have an ArrayList of Strings for each number in the file
         // do Advent 2024 day 4!
+    }
+
+    public static int checkXMas(ArrayList<ArrayList<String>> wordSearch) {
+        int count = 0;
+        ArrayList<String> arrayOfWord = convertWordToStrings("MAS");                                   //       M S        M M     S S         S M
+        ArrayList<String> reverseOfArrayOfWord = convertWordToStrings(reverse("MAS"));           //        A          A       A           A
+        for (int i = 0; i <= wordSearch.size() - arrayOfWord.size(); i++) {                            //       M S         S S     M M         S M
+            for (int j = 0; j <= wordSearch.get(i).size() - arrayOfWord.size(); j++) {
+                boolean issue = false;
+                for (int k = 0; k < arrayOfWord.size(); k++) {
+                    if (!arrayOfWord.get(k).equals(wordSearch.get(i + k).get(j + k))) {
+                        issue = true;
+                        break;
+                    }
+                }
+                if (issue) {
+                    issue = false;
+                    for (int k = 0; k < arrayOfWord.size(); k++) {
+                        if (!reverseOfArrayOfWord.get(k).equals(wordSearch.get(i + k).get(j + k))) {
+                            issue = true;
+                            break;
+                        }
+                    }
+                }
+                if (!issue) {
+                    for (int k = 0; k < arrayOfWord.size(); k++) {
+                        if (!arrayOfWord.get(k).equals(wordSearch.get(i + 2 - k).get(j + k))) {
+                            issue = true;
+                            break;
+                        }
+                    }
+                    if (issue) {
+                        issue = false;
+                        for (int k = 0; k < arrayOfWord.size(); k++) {
+                            if (!reverseOfArrayOfWord.get(k).equals(wordSearch.get(i + 2 - k).get(j + k))) {
+                                issue = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (!issue) {
+                    count++;
+                }
+            }
+            
+        }
+        return count;
     }
 
 
@@ -43,9 +91,7 @@ public class Day4 {
         for (int i = 0; i < wordSearch.size(); i++) { //go through each line of our 2d array of strings accounting for bounds issues
             for (int j = 0; j <= wordSearch.get(i).size() - arrayOfWord.size(); j++) { //go through each character in the line
                 boolean issue = false;  //checks for issues
-                System.out.println("go into");
                 for (int k = 0; k < arrayOfWord.size(); k++) { //checks the character selected and the next 3 characters to the right
-                    System.out.println(arrayOfWord.get(k) + ":" + wordSearch.get(i).get(j + k));
                     if (!arrayOfWord.get(k).equals(wordSearch.get(i).get(j + k))) {
                         issue = true;
                         break;
@@ -82,11 +128,11 @@ public class Day4 {
     //checks 'Xmas' diagonally right(\)
     public static int checkDiagonallyRight(ArrayList<String> arrayOfWord, ArrayList<ArrayList<String>> wordSearch) {
         int count = 0;
-        for (int i = 0; i < wordSearch.getFirst().size() - arrayOfWord.size(); i++) { //go through each column of our 2d array of strings accounting for bounds issues
-            for (int j = 0; j <= wordSearch.size() - arrayOfWord.size(); j++) { //go through each character in the column
+        for (int i = 0; i <= wordSearch.size() - arrayOfWord.size(); i++) { //go through each line of our 2d array of strings accounting for bounds issues
+            for (int j = 0; j <= wordSearch.get(i).size() - arrayOfWord.size(); j++) { //go through each character in the line
                 boolean issue = false;  //checks for issues
-                for (int k = 0; k < arrayOfWord.size(); k++) { //checks the character selected and the next 3 characters to the bottom and right of the previous
-                    if (!arrayOfWord.get(k).equals(wordSearch.get(j + k).get(i + k))) {
+                for (int k = 0; k < arrayOfWord.size(); k++) { //checks the character selected and the next 3 characters to the right
+                    if (!arrayOfWord.get(k).equals(wordSearch.get(i + k).get(j + k))) {
                         issue = true;
                         break;
                     }
